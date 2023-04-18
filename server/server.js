92576f9e1d271db9e1c406b9842eabcdfb1e5351
RickyWang1020 <xw1744@nyu.edu>
2023-04-18 18:44:44 -0400

    fix server routing

--- server/server.js
+++ server/server.js
@@ -19,24 +19,24 @@ app.get('/', (req, res) => {
 app.get('/top_movies', routes.top_movies);
 app.get('/movie_page', routes.movie_page);
 app.get('/movie_page/:movie_id', routes.movie);
-app.get('/movie_page/movie_id_distribution/:movie_id', routes.movie_rating_distribution);
-app.get('/movie_page/genre_defeat/:movie_id', routes.movie_defeat);
-app.get('/movie_page/filter_movies', routes.filter_movies);
-app.get('/popup/top_genres/:rate_bar', routes.count_top_genres);
-app.get('/popup/genre_rating_change/:genre?', routes.genre_rating_change);
-app.get('/popup/genre_runtime_change/:genre?', routes.genre_runtime_change);
-app.get('/popup/movie_after/:year?', routes.select_movies);
+app.get('/movie_id_distribution/:movie_id', routes.movie_rating_distribution);
+app.get('/genre_defeat/:movie_id', routes.movie_defeat);
+app.get('/filter_movies', routes.filter_movies);
+app.get('/top_genres/:rate_bar', routes.count_top_genres);
+app.get('/genre_rating_change/:genre?', routes.genre_rating_change);
+app.get('/genre_runtime_change/:genre?', routes.genre_runtime_change);
+app.get('/movie_after/:year?', routes.select_movies);
 
 //routes for cast part
 app.get('/top_cast', routes.top_cast);
 app.get('/top_cast_rating', routes.top_cast_rating)
-app.get('/cast_page/cast', routes.cast_page)
+app.get('/cast_page', routes.cast_page)
 app.get('/cast_page/:nconst', routes.cast)
-app.get('/cast_page/cast_filter', routes.cast_filter)
-app.get('/cast_page/top_writer', routes.top_writer)
-app.get('/cast_page/writer_filter', routes.writer_filter)
-app.get('/cast_page/top_director', routes.top_director)
-app.get('/cast_page/director_filter', routes.director_filter)
+app.get('/cast_filter', routes.cast_filter)
+app.get('/top_writer', routes.top_writer)
+app.get('/writer_filter', routes.writer_filter)
+app.get('/top_director', routes.top_director)
+app.get('/director_filter', routes.director_filter)
 
 
 app.listen(config.server_port, () => {
